50c1bdb630a670c15ae70ae63a22648f4d9a30ed
James Prestwich <james@prestwi.ch>
2018-05-24 20:31:33 -0700

    update readme, enforce compact VarInts

--- riemann/tx/tx.py
+++ riemann/tx/tx.py
@@ -191,6 +191,9 @@ class VarInt(ByteData):
             num = num[1:5]
         elif num[0] == 0xff:
             num = num[1:9]
+        if 'zcash' in riemann.get_current_network_name() and num[-1] == 0:
+            raise ValueError('VarInt must be compact. Got: {}'
+                             .format(byte_string.hex()))
         if len(num) == 0:
             raise ValueError('Malformed VarInt. Got: {}'
                              .format(byte_string.hex()))
