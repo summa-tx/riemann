50ab2d1c82d5da86d209cfe5c704940d5b0d3c32
Rachel <rachel@summa.one>
2018-07-12 13:29:01 -0700

    signed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='riemann-tx',
-    version='1.0.1',
+    version='1.0.2',
     description=('Transaction creation library for Bitcoin-like coins'),
     url='https://github.com/summa-tx/riemann',
     author='James Prestwich',
